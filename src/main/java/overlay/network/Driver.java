49d0a31ad5c6bb0b3a9c2b180d9a572754ce6fde
Manuel Porto <manu.porto94@gmail.com>
2019-03-20 21:03:54 +0100

    Add methods to class

--- src/main/java/overlay/network/Driver.java
+++ src/main/java/overlay/network/Driver.java
@@ -5,27 +5,40 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
 public class Driver {
 
+    private String exchangeName;
     private ConnectionFactory factory;
     private Connection connection;
     private Channel channel;
+    private String queueName;
 
-    public Driver(String host, int port) throws IOException, TimeoutException {
+    public Driver(String host, int port, String exchangeName) throws IOException, TimeoutException {
+        this.exchangeName = exchangeName;
         factory = new ConnectionFactory();
         factory.setHost(host);
         factory.setPort(port);
         try(Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
             this.connection = connection;
             this.channel = channel;
-            // TODO set proper exchange name
-            this.channel.exchangeDeclare("exchange-name", "direct");
+            this.channel.exchangeDeclare(exchangeName, "direct");
+            queueName = this.channel.queueDeclare().getQueue();
         }
     }
 
-    public void addConnection(String name) {
-
+    public void addConnection(String name) throws IOException {
+        channel.queueBind(queueName, exchangeName, name);
     }
+
+    public void send(String destination, String message) throws IOException {
+        channel.basicPublish(
+                exchangeName,
+                destination,
+                null,
+                message.getBytes(StandardCharsets.UTF_8));
+    }
+
 }
