fd41f9675b0b701c92e8df8b0b1f9359f9d37687
Manuel Porto <manu.porto94@gmail.com>
2019-03-29 17:09:26 +0100

    Add tests

--- src/test/java/overlay/util/BreadthFirstSearchTest.java
+++ src/test/java/overlay/util/BreadthFirstSearchTest.java
@@ -4,36 +4,49 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+// TODO refactor to parametrized test
 public class BreadthFirstSearchTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void testGetShortestDistancePath1() {
         int[][] adj = {
-                {0, 1, 0, 1},
-                {1, 0, 1, 0},
-                {0, 1, 0, 0},
-                {1, 0, 0, 0}};
-        int[] srcs = {0, 1, 2, 3};
+                {0, 1, 0, 1, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1, 0, 0, 1},
+                {0, 0, 0, 1, 0, 1, 1, 1},
+                {0, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 1, 1, 0, 1, 0}};
+        int src = 0;
+        int dest = 7;
+        int vertices = 8;
+        List<Integer> expected = Arrays.asList(0, 3, 7);
+        List<Integer> result = BreadthFirstSearch.getShortestDistance(adj, vertices, src, dest);
+
+        assertEquals(expected, result);
     }
 
     @Test
-    public void testGetShortestDistance() {
+    public void testGetShortestDistancePath2() {
         int[][] adj = {
-                {0, 1, 0, 1},
-                {1, 0, 1, 0},
-                {0, 1, 0, 0},
-                {1, 0, 0, 0}};
-        int src = 0;
-        int dest = 1;
-        int vertices = 4;
-        List<Integer> expected = new ArrayList<>(2);
-        expected.add(0);
-        expected.add(1);
-        //expected.add(2);
+                {0, 1, 0, 1, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1, 0, 0, 1},
+                {0, 0, 0, 1, 0, 1, 1, 1},
+                {0, 0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 1, 1, 0, 1, 0}};
+        int src = 2;
+        int dest = 6;
+        int vertices = 8;
+        List<Integer> expected = Arrays.asList(2, 1, 0, 3, 4, 6);
         List<Integer> result = BreadthFirstSearch.getShortestDistance(adj, vertices, src, dest);
 
         assertEquals(expected, result);
