3a720ffd2cc2894d89a255561196901f7944a42b
Manuel Porto <manu.porto94@gmail.com>
2019-03-31 11:12:03 +0200

    Add routing to virtual router

--- src/main/java/overlay/network/virtual/VirtualRouter.java
+++ src/main/java/overlay/network/virtual/VirtualRouter.java
@@ -1,37 +1,47 @@
 package overlay.network.virtual;
 
 import overlay.external.ExternalMessage;
+import overlay.util.BreadthFirstSearch;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class VirtualRouter implements Runnable {
+    private final int myID;
+    private Map<Integer, Integer> nextHopsForDestinations;
     private ConcurrentLinkedQueue<ExternalMessage> externalMessages;
     private ConcurrentLinkedQueue<Message> incomingMessages;
     private ConcurrentLinkedQueue<Message> outgoingMessages;
 
-    public VirtualRouter(ConcurrentLinkedQueue<ExternalMessage> externalMessages, ConcurrentLinkedQueue<Message> incomingMessages, ConcurrentLinkedQueue<Message> outgoingMessages) {
+    public VirtualRouter(ConcurrentLinkedQueue<ExternalMessage> externalMessages,
+                         ConcurrentLinkedQueue<Message> incomingMessages,
+                         ConcurrentLinkedQueue<Message> outgoingMessages) {
+        this.myID = -1;
+        int[][] adj = {{}}; // TODO get actual matrix info from somewhere
+        this.nextHopsForDestinations = BreadthFirstSearch.calculateNextHops(myID, adj, 0);
         this.externalMessages = externalMessages;
         this.incomingMessages = incomingMessages;
         this.outgoingMessages = outgoingMessages;
     }
 
     private void listenForExternalMessages() {
-        while(!Thread.interrupted()) {
+        while(!Thread.currentThread().isInterrupted()) {
             ExternalMessage externalMessage = externalMessages.remove();
-            // TODO get correct ids
-            Message newMessage = new Message(-1, externalMessage.getDest(), -1, externalMessage.getMessage());
+            int dest = externalMessage.getDest();
+            Message newMessage = new Message(myID, dest, nextHopsForDestinations.get(dest), externalMessage.getMessage());
             outgoingMessages.add(newMessage);
         }
     }
 
     private void listenForIncomingMessages() {
-        while (!Thread.interrupted()) {
+        while (!Thread.currentThread().isInterrupted()) {
             Message incomingMessage = incomingMessages.remove();
-            if (incomingMessage.getDest() == -1) { // TODO get my node id
+            if (incomingMessage.getDest() == myID) { // TODO get my node id
                 // TODO send to messagePrinter class
                 System.out.println(incomingMessage.toString());
             } else {
-                // TODO reroute the message
+                int nextHop = nextHopsForDestinations.get(incomingMessage.getDest());
+                incomingMessage.setNextHop(nextHop);
                 outgoingMessages.add(incomingMessage);
             }
         }
@@ -39,8 +49,16 @@ public class VirtualRouter implements Runnable {
 
     @Override
     public void run() {
-        // TODO check if i need to interrupt this thread when the main one (I) gets interrupted
-        new Thread(this::listenForExternalMessages).start();
-        new Thread(this::listenForIncomingMessages).start();
+        Thread externalMessagesListener = new Thread(this::listenForExternalMessages);
+        externalMessagesListener.start();
+        listenForIncomingMessages();
+
+        externalMessagesListener.interrupt();
+        try {
+            externalMessagesListener.join();
+        } catch (InterruptedException e) {
+            System.err.println("Couldn't properly join thread");
+            e.printStackTrace();
+        }
     }
 }
