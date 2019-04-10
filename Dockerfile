6d61cb37c7569dd0c3b1e215fa5962da401a4bc1
Manuel Porto <manu.porto94@gmail.com>
2019-04-10 10:39:10 +0200

    Delete unused files

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:jre-slim
-
-RUN mkdir /app
-COPY build/distributions/overlay.zip /app
-WORKDIR /app
-RUN unzip overlay.zip -d .
-CMD ./overlay/bin/overlay $MODE
--- res/loader.py
+++
@@ -1,9 +0,0 @@
-import os
-import zipfile
-
-os.system('./gradlew distZip')
-
-with zipfile.ZipFile("build/distributions/overlay.zip", "r") as zip_file:
-    zip_file.extractall("build/tmp/")
-
-os.system('./build/tmp/overlay/bin/overlay 0')
